9fb3c746ec81f2e123e80b12c60d94345b441369
JinKi Lee <63500833+lxxjxxki@users.noreply.github.com>
2024-01-07 18:46:14 +0900

    Time: 2 ms (74.40%), Space: 45.8 MB (30.35%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int len = nums.length;
+        int idx = 0;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != 0) 
+                nums[idx++] = nums[i];
+        }
+        Arrays.fill(nums, idx, len, 0);
+    }
+}
